088f59213adecd8f6b883db4c7eaa40c14da8fea
johann Imbett <gazabon.johann@gmail.com>
2024-10-06 15:21:13 +0200

    Fix syntax error in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,11 +43,12 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :cloudinary
-  
-  heroku config:set CLOUDINARY_CLOUD_NAME=dgdtyztpm
-  heroku config:set CLOUDINARY_API_KEY=753587871796867
-  heroku config:set CLOUDINARY_API_SECRET=6LZFOzXRJ9j9grklC09tUIk2MWU
 
+  config.cloudinary = {
+    cloud_name: 'dgdtyztpm',                            # Your Cloudinary cloud name
+    api_key: '753587871796867',                         # Your Cloudinary API key
+    api_secret: '6LZFOzXRJ9j9grklC09tUIk2MWU'          # Your Cloudinary API secret
+  }
 
 
   # Mount Action Cable outside main process or domain.
